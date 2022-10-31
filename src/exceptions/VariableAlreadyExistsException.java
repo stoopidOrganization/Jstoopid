0e8e13e8970c509b4c81b5df7748711c29a801f6
nils865 <nils.staas0806@gmail.com>
2022-10-31 13:06:24 +0100

    MORE DOCUMENTATION

--- src/exceptions/VariableAlreadyExistsException.java
+++ src/exceptions/VariableAlreadyExistsException.java
@@ -1,8 +1,11 @@
 package exceptions;
 
+/**
+ * Can be thrown when a variable with given name already exists
+ */
 public class VariableAlreadyExistsException extends Exception {
     /**
-     * @param name that isnt any type
+     * @param name of the variable
      */
     public VariableAlreadyExistsException(String name) {
         super("A Variable with the name: \'" + name + "\' already exists.");
